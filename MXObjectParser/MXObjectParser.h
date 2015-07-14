20c8f02951a08e0904a9ad575235495ad9d75d70
eric <longminxiang@163.com>
2015-07-14 14:58:56 +0800

    add object file

---
+++ MXObjectParser/MXObjectParser.h
@@ -0,0 +1,13 @@
+//
+//  MXObjectParser.h
+//  MXObjectParserDemo
+//
+//  Created by eric on 15/7/14.
+//  Copyright (c) 2015年 eric. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MXObjectParser : NSObject
+
+@end
---
+++ MXObjectParser/MXObjectParser.m
@@ -0,0 +1,13 @@
+//
+//  MXObjectParser.m
+//  MXObjectParserDemo
+//
+//  Created by eric on 15/7/14.
+//  Copyright (c) 2015年 eric. All rights reserved.
+//
+
+#import "MXObjectParser.h"
+
+@implementation MXObjectParser
+
+@end
